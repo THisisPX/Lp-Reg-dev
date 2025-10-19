2b97baf9873d9dc01554a6bf98ef66570aa5373c
CarlanLark <49721976+CarlanLark@users.noreply.github.com>
2025-10-19 19:00:32 +0000

    prevent OOM

--- recipe/lp_reg/Qwen2.5_32b_lp_reg_onpolicy_64gpu.sh
+++ recipe/lp_reg/Qwen2.5_32b_lp_reg_onpolicy_64gpu.sh
@@ -45,7 +45,7 @@ train_prompt_bsz=256
 gen_prompt_bsz=256
 train_prompt_mini_bsz=256
 n_resp_per_prompt=8
-max_token=$((1024 * 30))
+max_token=$((1024 * 20))
 
 # Ray
 RAY_ADDRESS=${RAY_ADDRESS:-"http://localhost:8265"}
